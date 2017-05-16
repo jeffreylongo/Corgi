8f9066e8402b7de5eb595f6f8cced3313fcf3758
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-16 16:03:11 -0400

    trying to add pocos for newsFeed

---
+++ Corgi/DataContext/NewsContext.cs
@@ -0,0 +1,20 @@
+﻿using Corgi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Corgi.DataContext
+{
+    public class NewsContext : DbContext
+    {
+        public NewsContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<NewsFeed> News { get; set; }
+    }
+
+}
---
+++ Corgi/Models/NewsFeed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Corgi.Models
+{
+    public class NewsFeed
+    {
+        public int Id { get; set; }
+        public string StoryName { get; set; }
+        public int HerdId { get; set; }
+        public string Url { get; set; }
+    }
+}

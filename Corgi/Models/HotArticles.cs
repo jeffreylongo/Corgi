a295bd85b5e84c2cfe36e2c45f4143276bc8e7d3
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-22 14:22:39 -0400

    I dont think I changed much, but apparently have made 10 changes...so commit.

---
+++ Corgi/Models/HotArticles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Corgi.Models
+{
+    public class HotArticles
+    {
+        public class Article
+        {
+            public int Id { get; set; }
+            public string StoryName { get; set; }
+            public string Url { get; set; }
+
+            public int NewsFeedId { get; set; }
+            public NewsFeed NewsFeed { get; set; }
+        }
+    }
+}

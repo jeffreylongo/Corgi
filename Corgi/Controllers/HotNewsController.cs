f5f4f9f3e6cd9780a734cfea566d6f50d73bd18f
Jeff Longo <jeffreylongo@yahoo.com>
2017-05-22 15:36:32 -0400

    added hotArticles model and HotNewsController.

---
+++ Corgi/Controllers/HotNewsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Corgi.Controllers
+{
+    public class HotNewsController : Controller
+    {
+        // GET: HotNews
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
